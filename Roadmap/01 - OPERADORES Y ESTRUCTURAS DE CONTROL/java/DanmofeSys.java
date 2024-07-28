25513d7c7db8c63ef51d6ee0ae138a37ae0fa8fa
danmofe-sys <danmofe@gmail.com>
2024-07-28 07:21:41 -0600

    Create DanmofeSys.java

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/DanmofeSys.java
@@ -0,0 +1,33 @@
+//Sitio web oficial de Java: https://www.java.com/
+
+//Sintaxis de comentario en una linea
+/*Sintaxis de comentario
+en varias lineas
+*/
+
+
+public class DanmofeSys {
+	public static void main(String[] args){
+
+	//Tipos de datos primitivos
+
+	String txt = "Hola mundo"; //String (cadena de texto)
+	int num = 20; //int (números de tipo entero)
+	boolean boolean = true; //boleano (verdadero o falso)
+	double decimal = 6.5; // numeros decimales con doble precision
+	float flotante = 6.5f; // numeros decimales de presicion simple
+	char caracter = 'd'; // caracter simple
+
+	// Variables
+	int num = 15;
+
+	// constante
+	final double pi = 3.14;
+
+	//imprimir por terminal
+	System.out.println("¡Hola Java!");
+
+	}
+
+
+}
